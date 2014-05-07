2819980247cbea29aa40d0aafb6fe32aacafc147
Sroop <sroopsunar@hotmail.com>
2014-05-07 13:36:37 +0100

    Database cleaner added to clear the database after every test run

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.1.1'
 gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'database_cleaner'
 
 group :development, :test do
 	gem 'rspec'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,3 +17,23 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = 'random'
 end
+
+require 'database_cleaner'
+
+RSpec.configure do |config|
+
+	config.before(:suite) do
+		DatabaseCleaner.strategy = :transaction
+		DatabaseCleaner.clean_with(:truncation)
+	end
+
+	config.before(:each) do
+		DatabaseCleaner.start
+	end
+
+	config.after(:each) do
+		DatabaseCleaner.clean
+	end
+
+end
+
