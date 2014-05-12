ee4b5bcdbd15892e52af39f133fb21beb0790b15
Sroop <sroopsunar@hotmail.com>
2014-05-12 19:41:45 +0100

    error fixed, flash created for mismatched passwords and duplicate email addresses

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -15,13 +15,13 @@ feature "User signs up" do
 	scenario "with a password that doesn't match" do
 		lambda { sign_up('a@a.com', 'pass', 'wrong') }.should change(User, :count).by(0)
 		expect(current_path).to eq('/users')
-		expect(page).to have_content("Sorry, your passwords don't match")
+		expect(page).to have_content("Password does not match")
 	end
 
 	scenario "With an email that is already registered" do
 		lambda { sign_up }.should change(User, :count).by(1)
 		lambda { sign_up }.should change(User, :count).by(0)
-		expect(page).to have_content("Sorry, this email address is already taken")
+		expect(page).to have_content("Email is already taken")
 	end
 
 	def sign_up(email = "sroop@sunar.com",
